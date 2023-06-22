fdfb08a018cd5747619620e57684dec0e31aaebf
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-06-22 16:36:12 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+import { useState } from 'react';
+import { Formik } from 'formik';
+
+export default function App() {
+  const REGEX = {
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{6,}$/
+  };
+
+  const [form, setForm] = useState(0);
+
+  function handleChange(event) {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function handleValidate() {
+    const errors = {};
+    if (!form.email) {
+      errors.email = "Required";
+    } else if (!REGEX.email.test(form.email)) {
+      errors.email = "Invalid email address";
+    }
+    if (!form.password) {
+      errors.password = "Required";
+    } else if (!REGEX.password.test(form.password)) {
+      errors.password = "Unenough 6 charactor";
+    }
+    return errors;
+  }
+
+  function handleSubmit() {
+    alert("Login in successfully!!!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sign up</h1>
+      <Formik initialValues={form}
+              validate={handleValidate}
+              onSubmit={handleSubmit}>
+        {({ errors, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <div
+              className={`custom-input ${
+                errors.email ? "custom-input-error" : ""
+              }`}
+            >
+              <label>Email</label>
+              <input
+                name="email"
+                value={form.email || ""}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.email}</p>
+            </div>
+            <div
+              className={`custom-input ${
+                errors.password ? "custom-input-error" : ""
+              }`}
+            >
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                value={form.password || ""}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.password}</p>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
-
-export default App;
